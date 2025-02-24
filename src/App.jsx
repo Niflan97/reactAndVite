dd702023dddd479b50ee0e1d9096757783a0581c
mohamed Niflan <niflannif2@gmail.com>
2025-02-24 12:20:03 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { FaAngleDoubleDown, FaAngleDown } from 'react-icons/fa'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>
+          This project made by React and Vite
+        </h1>
+      </div>
+      
+      <div className="card">
+        <p>Click here to increase count</p>
+        <FaAngleDoubleDown/><br/>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          <code>niflanMHD</code> 
+        </p>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
